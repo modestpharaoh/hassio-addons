d0c3be11eb858321b04863dbbf58e606d7aabeb2
modestpharaoh <30378999+modestpharaoh@users.noreply.github.com>
2023-02-19 19:35:55 +0000

    call  draw-hass-dash-service-2023-portrait.py
    gunicorn_start_kobo_dash to call  draw-hass-dash-service-2023-portrait.py

--- kobo_dash/gunicorn_start_kobo_dash.sh
+++ kobo_dash/gunicorn_start_kobo_dash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --chdir /app draw-hass-dash-service:app -w 1 --threads 2 -b 0.0.0.0:5006 --log-file=- --limit-request-line 0
+gunicorn --chdir /app draw-hass-dash-service-2023-portrait:app -w 1 --threads 2 -b 0.0.0.0:5006 --log-file=- --limit-request-line 0
