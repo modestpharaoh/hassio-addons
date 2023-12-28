d494e401d60851eddc67a8ea475af92c79225e3a
modestpharaoh <modestpharaoh@gmail.com>
2023-12-28 12:53:25 +0000

    Improve the error handling and code comments

--- multi-ddns/app/cleanup_script.sh
+++ multi-ddns/app/cleanup_script.sh
@@ -4,6 +4,8 @@ source /api_lib.sh
 
 # bashio::log.level "debug"
 
+# This script is called during the authentication phase to clear the DNS TXT record.
+
 DUCKDNS_TOKEN=${Duck_Token}
 DYNU_TOKEN=${Dynu_Token}
 domain="$CERTBOT_DOMAIN"
@@ -14,5 +16,11 @@ TXT_VALUE="$CERTBOT_VALIDATION"
 if [[ $domain == *"duckdns.org" ]]; then
     rm_duck_txt_record "$domain" "$DUCKDNS_TOKEN"
 else
-    rm_dynu_txt_record "$domain" "$DYNU_TOKEN"
+    # Getting the Dynu DNS domain IDs
+    DOMAINID=$(get_dynu_domain_id $domain $DYNU_TOKEN)
+    if [ $? -eq 0 ]; then
+        rm_dynu_txt_records "$domain" "$DYNU_TOKEN" "$DOMAINID"
+    else
+        bashio::log.warning "Domain $domain: Can't delete TXT DNS records without the domain ID!!!"
+    fi
 fi
