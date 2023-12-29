ea0bf32fed007864cbdd4373ef822318cad06c71
modestpharaoh <modestpharaoh@gmail.com>
2023-12-29 03:32:05 +0000

    small update

--- multi-ddns/app/run.sh
+++ multi-ddns/app/run.sh
@@ -114,7 +114,7 @@ function le_renew() {
       --register-unsafely-without-email --agree-tos \
       --deploy-hook /deploy_hook.sh \
       --non-interactive \
-      ${main_domain_args[@]} # || bashio::log.warning "Add-on hit the limit of let's encrypt updates!!"
+      ${main_domain_args[@]}
     renew_exit_code=$?
     
 
@@ -133,7 +133,7 @@ function le_renew() {
           --register-unsafely-without-email --agree-tos \
           --deploy-hook /deploy_hook.sh \
           --non-interactive \
-          ${domain_args[@]} # || bashio::log.warning "Add-on hit the limit of let's encrypt updates!!"
+          ${domain_args[@]}
         if [ $? -ne 0 ]; then
           bashio::log.warning "Certbot expand exit code:" "$?"
         fi
